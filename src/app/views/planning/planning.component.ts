61babeb292ad05f492b8d59b895822d4a4ec8e89
Beatriz Lopez <beatlm@gmail.com>
2018-07-02 23:40:41 +0200

    planning v1

--- src/app/views/planning/planning.component.ts
+++ src/app/views/planning/planning.component.ts
@@ -1,19 +1,28 @@
+import { DayModel } from './../../services/model/dayModel';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mr-planning',
-  template: `
-    <p>
-      planning works!
-    </p>
-  `,
+  templateUrl: './planning.html',
   styles: []
 })
 export class PlanningComponent implements OnInit {
+public  week: DayModel[];
 
-  constructor() { }
+constructor() { }
 
   ngOnInit() {
+   
+    this.week=[new DayModel("Lunes",true,false,"344","Judias")];
+    this.week.push(new DayModel("Martes",false,false,"344","Gazpacho"));
+    this.week.push(new DayModel("Miércoles",false,false,"344","Paella"));
+    this.week.push(new DayModel("Jueves",false,false,"344","Ensalada"));
+    this.week.push(new DayModel("Viernes",false,true,"344","Piña"));
+    this.week.push(new DayModel("Sábado",false,false,"344","Judias"));
+    this.week.push(new DayModel("Domingo",false,false,"344","Judias"));
   }
 
+  public selectDay(title:string){
+    alert(title);
+  }
 }

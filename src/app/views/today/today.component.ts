62a53dd8b0cdf1bb96d5216e29d4a8f8a609f6a2
Beatriz Lopez <beatlm@gmail.com>
2018-09-02 00:20:28 +0200

    Pagina hoy

--- src/app/views/today/today.component.ts
+++ src/app/views/today/today.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../../services/recipes.service';
+import { AlertService } from '../../services/alert.service';
+import { Router } from '@angular/router';
+import { LoaderService } from '../../services/loader.service';
+import { RecipeModel } from '../../services/RecipeModel';
 
 @Component({
   selector: 'mr-today',
-  template: `
-    <p>
-      today works!
-    </p>
-  `,
+  templateUrl:'./today.html',
   styles: []
 })
 export class TodayComponent implements OnInit {
-
-  constructor() { }
+  public recipes: RecipeModel[];
+  constructor(  private recipesService: RecipesService,
+    public loaderService: LoaderService,
+    private router: Router,
+    private alertService: AlertService) { }
 
   ngOnInit() {
+    this.recipes=new RecipeModel[0];
   }
 
+  private buscarTag(tag:String){
+    this.recipesService
+    .getRecipesListByTag$(tag)
+    .subscribe(this.showRecipes.bind(this), this.catchError.bind(this));
+  }
+  private showRecipes(resultado: RecipeModel[]) {
+    this.loaderService.stopLoader();
+    this.recipes = resultado;
+  }
+
+  private catchError(err) {
+    this.loaderService.stopLoader();
+    this.alertService.create(
+      "Ha ocurrido un error" , //title
+      "danger", //type
+      2500 // time
+    );
+  }
 }
